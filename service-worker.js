098ccd0eb3185ea605623c3f6bd4af6cd4ab8573
Financetools <93783128+Mateistredie@users.noreply.github.com>
2026-06-07 02:16:17 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,16 @@
-const CACHE_NAME = "finance-tools-ro-v9";
-self.addEventListener("install", event => { self.skipWaiting(); });
-self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))); self.clients.claim(); });
-self.addEventListener("fetch", event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
+const CACHE_NAME = "finance-tools-ro-9-6-final";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
