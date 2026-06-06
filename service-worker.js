d55e4d6e7ab237e49d01c09d35ec727dccfbd65c
Financetools <93783128+Mateistredie@users.noreply.github.com>
2026-06-06 13:35:27 +0300

    Update service worker cache name and logic
    Updated cache name and modified cache handling logic during activation and fetch events.

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,22 @@
-const CACHE_NAME = "finance-tools-ro-v1";
-
-const URLS_TO_CACHE = [
-  "/Financetools/",
-  "/Financetools/index.html",
-  "/Financetools/style.css"
-];
+const CACHE_NAME = "finance-tools-ro-v2";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
