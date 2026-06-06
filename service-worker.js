437a6cb41462b10bb5329d82c5dd833445d54271
Financetools <93783128+Mateistredie@users.noreply.github.com>
2026-06-06 10:57:24 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,21 +3,20 @@ const CACHE_NAME = "finance-tools-ro-v1";
 const URLS_TO_CACHE = [
   "/Financetools/",
   "/Financetools/index.html",
-  "/Financetools/style.css",
-  "/Financetools/manifest.json"
+  "/Financetools/style.css"
 ];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
